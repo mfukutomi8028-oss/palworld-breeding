a1b998c50bed25eee87371130a974545d9617703
mfukutomi8028-oss <m.fukutomi8028@gmail.com>
2026-06-26 14:59:09 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,12 @@
 // 友人とリアルタイム共同編集したい場合は、Firebaseの設定値を下に貼り付けてください。
 // 設定例は config.example.js を見てください。
 
-window.firebaseConfig = null;
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
