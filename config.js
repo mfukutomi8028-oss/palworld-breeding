e432e44b7173891cd4275fcad63585e5f07146a8
mfukutomi8028-oss <m.fukutomi8028@gmail.com>
2026-06-26 15:15:59 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
-// Firebaseを未設定のままにすると、ブラウザ内だけのローカル保存で動作します。
-// 友人とリアルタイム共同編集したい場合は、Firebaseの設定値を下に貼り付けてください。
-// 設定例は config.example.js を見てください。
+// Firebase設定
+// このファイルでは初期化処理は行いません。
+// app.js 側でこの設定を読み込んで Firebase に接続します。
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 window.firebaseConfig = {
   apiKey: "AIzaSyDNyaOvTa9TdiY9-8q00MD0JcRpJY4LX08",
   authDomain: "palworld-breeding-board.firebaseapp.com",
@@ -20,7 +12,3 @@ window.firebaseConfig = {
   appId: "1:657534681097:web:b031035357baa6df2a38d8",
   measurementId: "G-3SKBN4JL7B"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
