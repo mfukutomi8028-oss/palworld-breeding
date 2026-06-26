32e553c47eaa83b5f492e99ad960781e5be86b34
mfukutomi8028-oss <m.fukutomi8028@gmail.com>
2026-06-26 15:11:09 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,12 +2,25 @@
 // 友人とリアルタイム共同編集したい場合は、Firebaseの設定値を下に貼り付けてください。
 // 設定例は config.example.js を見てください。
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDNyaOvTa9TdiY9-8q00MD0JcRpJY4LX08",
+  authDomain: "palworld-breeding-board.firebaseapp.com",
+  databaseURL: "https://palworld-breeding-board-default-rtdb.firebaseio.com",
+  projectId: "palworld-breeding-board",
+  storageBucket: "palworld-breeding-board.firebasestorage.app",
+  messagingSenderId: "657534681097",
+  appId: "1:657534681097:web:b031035357baa6df2a38d8",
+  measurementId: "G-3SKBN4JL7B"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
